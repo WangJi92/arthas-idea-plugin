d73e01ff9972a023b9e6c299ac7706810d334a27
wangji <983433479@qq.com>
2019-12-23 23:33:09 +0800

    添加帮助文档链接

---
+++ src/com/command/idea/plugin/Action/arthas/HelpAction.java
@@ -0,0 +1,19 @@
+package com.command.idea.plugin.Action.arthas;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * 帮助文档链接
+ *
+ * @author jet
+ * @date 23-12-2019
+ */
+public class HelpAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        BrowserUtil.browse("https://github.com/WangJi92/arthas-idea-plugin/blob/master/README.md");
+    }
+}
