e77a6c4ce0fcd2d900985c7aa681a143a108d662
wangji <983433479@qq.com>
2020-03-22 01:55:20 +0800

    watch get spring context

---
+++ src/com/github/wangji92/arthas/plugin/action/arthas/ArthasWatchOgnlSpringContextInvokeMethodAction.java
@@ -0,0 +1,143 @@
+package com.github.wangji92.arthas.plugin.action.arthas;
+
+import com.github.wangji92.arthas.plugin.ui.ArthasActionWatchSpringContextDialog;
+import com.github.wangji92.arthas.plugin.utils.OgnlPsUtils;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author 汪小哥
+ * @date 22-03-2020
+ */
+public class ArthasWatchOgnlSpringContextInvokeMethodAction  extends AnAction {
+
+    /**
+     * watch 获取spring context 进行处理
+     */
+    private  static final String WATCH_SPRING_CONTEXT="watch -x 3 -n 1  org.springframework.web.servlet.DispatcherServlet doDispatch '@org.springframework.web.context.support.WebApplicationContextUtils@getWebApplicationContext(params[0].getServletContext()).getBean(\"%s\").%s'";
+
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+        DataContext dataContext = e.getDataContext();
+        Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+        if (editor == null) {
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+        //获取当前事件触发时，光标所在的元素
+        PsiElement psiElement = CommonDataKeys.PSI_ELEMENT.getData(dataContext);
+        if (psiElement == null) {
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+        if (psiElement instanceof PsiClass) {
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+
+        if (psiElement instanceof PsiField) {
+            PsiField psiField = (PsiField) psiElement;
+            if (psiField.hasModifierProperty(PsiModifier.STATIC)) {
+                e.getPresentation().setEnabled(false);
+                return;
+            }
+            e.getPresentation().setEnabled(true);
+            return;
+        }
+
+        //判断是否为静态方法
+        if (psiElement instanceof PsiMethod) {
+            /**
+             * {@link https://www.programcreek.com/java-api-examples/?class=com.intellij.psi.PsiField&method=hasModifierProperty }
+             */
+            PsiMethod psiMethod = (PsiMethod) psiElement;
+            if (psiMethod.hasModifierProperty(PsiModifier.STATIC)) {
+                e.getPresentation().setEnabled(false);
+                return;
+            }
+            //抽象方法不处理
+            if (psiMethod.hasModifierProperty(PsiModifier.ABSTRACT)) {
+                e.getPresentation().setEnabled(false);
+                return;
+            }
+            //默认方法不处理
+            if (psiMethod.hasModifierProperty(PsiModifier.DEFAULT)) {
+                e.getPresentation().setEnabled(false);
+                return;
+            }
+            //native 方法不处理
+            if (psiMethod.hasModifierProperty(PsiModifier.NATIVE)) {
+                e.getPresentation().setEnabled(false);
+                return;
+            }
+        }
+        e.getPresentation().setEnabled(true);
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        /**
+         * {@link com.intellij.ide.actions.CopyReferenceAction}
+         */
+        DataContext dataContext = event.getDataContext();
+        Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+        Project project = CommonDataKeys.PROJECT.getData(dataContext);
+        if (editor == null || project == null) {
+            return;
+        }
+        PsiElement psiElement = CommonDataKeys.PSI_ELEMENT.getData(dataContext);
+        String className = "";
+        PsiClass psiClass =null;
+
+        if (psiElement instanceof PsiClass) {
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder("");
+
+        //支持方法
+        if (psiElement instanceof PsiMethod) {
+            PsiMethod psiMethod = (PsiMethod) psiElement;
+            psiClass = psiMethod.getContainingClass();
+            className = psiMethod.getContainingClass().getQualifiedName();
+            String methodName = psiMethod.getNameIdentifier().getText();
+            //构建表达式
+            builder.append(methodName).append("(");
+            //处理参数
+            PsiParameter[] parameters = psiMethod.getParameterList().getParameters();
+            if (parameters.length > 0) {
+                int index = 0;
+                for (PsiParameter parameter : parameters) {
+                    String defaultParamValue = OgnlPsUtils.getDefaultString(parameter.getType());
+                    builder.append(defaultParamValue);
+                    if (!(index == parameters.length - 1)) {
+                        builder.append(",");
+                    }
+                    index++;
+                }
+            }
+            builder.append(")");
+
+        }
+
+        //支持field
+        if (psiElement instanceof PsiField) {
+            PsiField psiField = (PsiField) psiElement;
+            className = psiField.getContainingClass().getQualifiedName();
+            String fileName = psiField.getNameIdentifier().getText();
+            psiClass = psiField.getContainingClass();
+            //构建 field的信息
+            builder.append(fileName);
+        }
+
+        String lowCamelBeanName = OgnlPsUtils.getClassBeanName(psiClass);
+        String watchSpringOgnlExpression = String.format(WATCH_SPRING_CONTEXT, lowCamelBeanName, builder.toString());
+        new ArthasActionWatchSpringContextDialog(project, className, watchSpringOgnlExpression).open("arthas watch ognl get spring context invoke method field 要触发任意的接口调用");
+    }
+}
---
+++ src/com/github/wangji92/arthas/plugin/ui/ArthasActionWatchSpringContextDialog.java
@@ -0,0 +1,138 @@
+package com.github.wangji92.arthas.plugin.ui;
+
+import com.github.wangji92.arthas.plugin.utils.ClipboardUtils;
+import com.github.wangji92.arthas.plugin.utils.NotifyUtils;
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.ui.components.labels.ActionLink;
+import com.intellij.ui.components.labels.LinkLabel;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ArthasActionWatchSpringContextDialog extends JDialog {
+
+    private JButton closeButton;
+
+    private JTextField ognlExpressionEditor;
+
+    private JPanel contentPane;
+    private LinkLabel ognlOfficeLinkLabel;
+    private LinkLabel oglSpecialLink;
+    private LinkLabel ognlDemoLink;
+    private LinkLabel watchHelpLink;
+
+
+    private String className;
+
+    private String staticOgnlExpression;
+
+    private Project project;
+
+
+    public ArthasActionWatchSpringContextDialog(Project project, String className, String staticOgnlExpression) {
+        this.project = project;
+        setContentPane(this.contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(closeButton);
+        this.className = className;
+        this.staticOgnlExpression = staticOgnlExpression;
+
+        closeButton.addActionListener(e -> onOK());
+
+
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        init();
+    }
+
+
+    private void init() {
+        ognlExpressionEditor.setText(this.staticOgnlExpression);
+    }
+
+
+    /**
+     * 取人按钮回调
+     */
+    private void onOK() {
+        String ognCurrentExpression = ognlExpressionEditor.getText();
+        if (StringUtils.isNotBlank(ognCurrentExpression)) {
+            ClipboardUtils.setClipboardString(ognCurrentExpression);
+            NotifyUtils.notifyMessageDefault(project);
+        }
+        dispose();
+    }
+
+
+    /**
+     * 关闭
+     */
+    private void onCancel() {
+        dispose();
+    }
+
+    /**
+     * 打开窗口
+     */
+    public void open(String title) {
+        setTitle(title);
+        pack();
+        //两个屏幕处理出现问题，跳到主屏幕去了
+        setLocationRelativeTo(WindowManager.getInstance().getFrame(this.project));
+        setVisible(true);
+
+    }
+
+
+    private void createUIComponents() {
+        ognlOfficeLinkLabel = new ActionLink("", AllIcons.Ide.Link, new AnAction() {
+            @Override
+            public void actionPerformed(AnActionEvent anActionEvent) {
+                BrowserUtil.browse("https://commons.apache.org/proper/commons-ognl/language-guide.html");
+            }
+        });
+        ognlOfficeLinkLabel.setPaintUnderline(false);
+
+        oglSpecialLink = new ActionLink("", AllIcons.Ide.Link, new AnAction() {
+            @Override
+            public void actionPerformed(AnActionEvent anActionEvent) {
+                BrowserUtil.browse("https://github.com/alibaba/arthas/issues/71");
+            }
+        });
+        oglSpecialLink.setPaintUnderline(false);
+        ognlDemoLink = new ActionLink("", AllIcons.Ide.Link, new AnAction() {
+            @Override
+            public void actionPerformed(AnActionEvent anActionEvent) {
+                BrowserUtil.browse("https://github.com/WangJi92/arthas-idea-plugin/issues/5");
+            }
+        });
+        ognlDemoLink.setPaintUnderline(false);
+
+        //https://github.com/WangJi92/arthas-idea-plugin/issues/5
+
+        watchHelpLink = new ActionLink("", AllIcons.Ide.Link, new AnAction() {
+            @Override
+            public void actionPerformed(AnActionEvent anActionEvent) {
+                BrowserUtil.browse("   https://alibaba.github.io/arthas/watch");
+            }
+        });
+        ognlDemoLink.setPaintUnderline(false);
+    }
+}
