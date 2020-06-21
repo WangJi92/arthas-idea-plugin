dcae41ebe9db810d127b061030c8d8a14fee0ba8
wangji <983433479@qq.com>
2020-06-21 16:06:40 +0800

    功能增强：watch 获取字段的值

--- src/com/github/wangji92/arthas/plugin/action/arthas/ArthasWatchCommandAction.java
+++ src/com/github/wangji92/arthas/plugin/action/arthas/ArthasWatchCommandAction.java
@@ -5,6 +5,8 @@ import com.github.wangji92.arthas.plugin.utils.ClipboardUtils;
 import com.github.wangji92.arthas.plugin.utils.NotifyUtils;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiModifier;
 
 /**
  * watch展开结构
@@ -15,8 +17,22 @@ import com.intellij.psi.PsiElement;
 public class ArthasWatchCommandAction extends BaseArthasPluginAction {
     @Override
     public void doCommand(String className, String methodName, Project project, PsiElement psiElement) {
-        String command = String.join(" ", "watch", className, methodName, "'{params,returnObj,throwExp}'", "-n", ArthasCommandConstants.INVOKE_COUNT, "-x", ArthasCommandConstants.RESULT_X, ArthasCommandConstants.DEFAULT_CONDITION_EXPRESS);
+        StringBuilder watchContentBuilder = new StringBuilder("'{params,returnObj,throwExp");
+        //这里针对放置在字段上获取字段的值的信息进行处理增强
+        if (psiElement instanceof PsiField) {
+            PsiField psiField = (PsiField) psiElement;
+            String fieldName = psiField.getNameIdentifier().getText();
+            if (!psiField.hasModifierProperty(PsiModifier.STATIC)) {
+                //获取非静态字段的信息
+                watchContentBuilder.append(",target.").append(fieldName);
+            } else {
+                //watch 获取静态字段的值
+                watchContentBuilder.append(",@").append(className).append("@").append(fieldName);
+            }
+        }
+        watchContentBuilder.append("}'");
+        String command = String.join(" ", "watch", className, methodName, watchContentBuilder.toString(), "-n", ArthasCommandConstants.INVOKE_COUNT, "-x", ArthasCommandConstants.RESULT_X, ArthasCommandConstants.DEFAULT_CONDITION_EXPRESS);
         ClipboardUtils.setClipboardString(command);
-        NotifyUtils.notifyMessage(project, "支持ognl条件表达式(默认1==1) eg:'params[0].name=\"name\" and params.size == 1' eg:'returnObj instanceof java.lang.String && returnObj.length>5'");
+        NotifyUtils.notifyMessage(project, "支持表达式(默认1==1) eg:'params[0].name=\"name\" and params.size == 1',可以将光标放置咋字段上watch获取值 ");
     }
 }
