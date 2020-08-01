2c69c3a426c7c8de25d5af9ba5caa9ac3f9cb75d
wangji <983433479@qq.com>
2020-08-01 22:25:55 +0800

    功能增强：getstatic

--- src/com/github/wangji92/arthas/plugin/ui/ArthasSpecialDialog.java
+++ src/com/github/wangji92/arthas/plugin/ui/ArthasSpecialDialog.java
@@ -34,7 +34,7 @@ public class ArthasSpecialDialog extends JDialog {
     private LinkLabel conditionArthas;
     private LinkLabel watchSpringContext;
     private LinkLabel asyncExample;
-    private LinkLabel arthasBastUnderStand;
+    private LinkLabel arthasBestUnderStand;
 
     private Project project;
 
@@ -143,13 +143,13 @@ public class ArthasSpecialDialog extends JDialog {
             }
         });
         asyncExample.setPaintUnderline(false);
-        arthasBastUnderStand = new ActionLink("", AllIcons.Ide.Link, new AnAction() {
+        arthasBestUnderStand = new ActionLink("", AllIcons.Ide.Link, new AnAction() {
             @Override
             public void actionPerformed(AnActionEvent anActionEvent) {
                 BrowserUtil.browse("https://wangji.blog.csdn.net/article/details/106964278");
             }
         });
-        arthasBastUnderStand.setPaintUnderline(false);
+        arthasBestUnderStand.setPaintUnderline(false);
     }
 
     /**
