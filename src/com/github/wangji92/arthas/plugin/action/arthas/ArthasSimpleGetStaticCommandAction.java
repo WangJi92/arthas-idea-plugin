2559064bb84b45c3b5c1dcba373e68afccff5e70
wangji <983433479@qq.com>
2020-08-01 22:25:55 +0800

    功能增强：getstatic

---
+++ src/com/github/wangji92/arthas/plugin/action/arthas/ArthasSimpleGetStaticCommandAction.java
@@ -0,0 +1,76 @@
+package com.github.wangji92.arthas.plugin.action.arthas;
+
+import com.github.wangji92.arthas.plugin.constants.ArthasCommandConstants;
+import com.github.wangji92.arthas.plugin.utils.ClipboardUtils;
+import com.github.wangji92.arthas.plugin.utils.NotifyUtils;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiModifier;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * https://arthas.gitee.io/getstatic.html 简单的版本的获取静态变量
+ *
+ * @author 汪小哥
+ * @date 01-08-2020
+ */
+public class ArthasSimpleGetStaticCommandAction extends AnAction {
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+        DataContext dataContext = e.getDataContext();
+        Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+        if (editor == null) {
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+        //获取当前事件触发时，光标所在的元素
+        PsiElement psiElement = CommonDataKeys.PSI_ELEMENT.getData(dataContext);
+        if (psiElement == null) {
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+        if (psiElement instanceof PsiField) {
+            PsiField psiField = (PsiField) psiElement;
+            if (!psiField.hasModifierProperty(PsiModifier.STATIC)) {
+                e.getPresentation().setEnabled(false);
+                return;
+            } else {
+                //只支持static field
+                e.getPresentation().setEnabled(true);
+            }
+            return;
+        }
+
+        e.getPresentation().setEnabled(false);
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        DataContext dataContext = event.getDataContext();
+        Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+        Project project = CommonDataKeys.PROJECT.getData(dataContext);
+        if (editor == null || project == null) {
+            return;
+        }
+        PsiElement psiElement = CommonDataKeys.PSI_ELEMENT.getData(dataContext);
+        if (psiElement instanceof PsiField) {
+            PsiField psiField = (PsiField) psiElement;
+            if (!psiField.hasModifierProperty(PsiModifier.STATIC)) {
+                return;
+            }
+            String className = psiField.getContainingClass().getQualifiedName();
+            String fileName = psiField.getNameIdentifier().getText();
+            String command = String.join(" ", "getstatic",className,fileName, "-x", ArthasCommandConstants.RESULT_X);
+            ClipboardUtils.setClipboardString(command);
+            NotifyUtils.notifyMessage(project, "简单的获取静态变量的值 如果多个classloader 加载获取不到信息");
+        }
+    }
+}
