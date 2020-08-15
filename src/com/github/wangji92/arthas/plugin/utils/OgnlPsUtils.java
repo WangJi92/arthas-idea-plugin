16fbf4c455e3cc927d65418cc6fadf5f6d080cde
wangji <983433479@qq.com>
2020-08-16 00:11:28 +0800

    版本兼容性问题

--- src/com/github/wangji92/arthas/plugin/utils/OgnlPsUtils.java
+++ src/com/github/wangji92/arthas/plugin/utils/OgnlPsUtils.java
@@ -1,15 +1,12 @@
 package com.github.wangji92.arthas.plugin.utils;
 
 import com.github.wangji92.arthas.plugin.constants.ArthasCommandConstants;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.intellij.psi.*;
-import org.apache.commons.compress.utils.Lists;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 获取Java类型构造ognl的默认值信息
