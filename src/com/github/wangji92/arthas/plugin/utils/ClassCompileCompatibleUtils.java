5f33f7aa3c63354f067c76e72215f523a8185497
汪吉 <983433479@qq.com>
2021-05-29 16:34:31 +0800

    compatible 2021

--- src/com/github/wangji92/arthas/plugin/utils/ClassCompileCompatibleUtils.java
+++ src/com/github/wangji92/arthas/plugin/utils/ClassCompileCompatibleUtils.java
@@ -2,6 +2,7 @@ package com.github.wangji92.arthas.plugin.utils;
 
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.task.ProjectTaskManager;
@@ -9,6 +10,7 @@ import com.intellij.task.ProjectTaskNotification;
 import org.apache.commons.lang3.reflect.MethodUtils;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.function.Consumer;
 
 /**
  * 编译工程 代码兼容方案
@@ -17,6 +19,8 @@ import java.lang.reflect.InvocationTargetException;
  * @date 27-05-2021
  */
 public class ClassCompileCompatibleUtils {
+    private static final Logger LOG = Logger.getInstance(ClassCompileCompatibleUtils.class);
+
     /**
      * 编译兼容的方案
      *
@@ -26,7 +30,7 @@ public class ClassCompileCompatibleUtils {
      */
     public static void compile(Project project, VirtualFile[] virtualFileFiles, Runnable successRunnable) {
         if (ApplicationInfo.getInstance().getBuild().getBaselineVersion() <= 201) {
-            //2018.2 编译报错
+            //2021 版本开发删除掉下面的代码 
             WriteActionCompatibleUtils.runAndWait(project, () -> {
                 ProjectTaskManager instance = ProjectTaskManager.getInstance(project);
                 ProjectTaskNotification taskNotification = projectTaskResult -> {
@@ -48,12 +52,23 @@ public class ClassCompileCompatibleUtils {
             WriteActionCompatibleUtils.runAndWait(project, () -> {
                 ProjectTaskManager instance = ProjectTaskManager.getInstance(project);
                 Object promise = MethodUtils.invokeMethod(instance, "compile", new Object[]{virtualFileFiles}, new Class[]{VirtualFile[].class});
-                Object isSucceeded = MethodUtils.invokeMethod(promise, "isSucceeded");
-                if (!Boolean.TRUE.equals(isSucceeded)) {
+                MethodUtils.invokeMethod(promise, "onSuccess", (Consumer) o -> {
+                    boolean result = false;
+                    try {
+                        result = (Boolean) MethodUtils.invokeExactMethod(o, "hasErrors");
+                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                        //ignore
+                    }
+                    if (Boolean.FALSE.equals(result)) {
+                        WriteActionCompatibleUtils.runAndWait(project, successRunnable::run);
+                    } else {
+                        NotifyUtils.notifyMessage(project, "File compilation errors (it's best to make sure you compile the whole project at least once before a hot update) This will only partially compile the current file", NotificationType.ERROR);
+                    }
+                });
+                MethodUtils.invokeMethod(promise, "onError", (Consumer<? super Throwable>) o -> {
+                    LOG.error("invoke runAndWait error", o.getCause());
                     NotifyUtils.notifyMessage(project, "File compilation errors (it's best to make sure you compile the whole project at least once before a hot update) This will only partially compile the current file", NotificationType.ERROR);
-                    return;
-                }
-                WriteActionCompatibleUtils.runAndWait(project, successRunnable::run);
+                });
             });
         }
     }
