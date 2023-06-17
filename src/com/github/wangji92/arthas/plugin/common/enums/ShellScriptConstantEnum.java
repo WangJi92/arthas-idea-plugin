4f85afeb0ec7fb9e2f66e6db438e9fe8a1131701
wangji <983433479@qq.com>
2023-06-18 00:00:53 +0800

    add static script vmtool interrupt thread by thread id(thread command get)

--- src/com/github/wangji92/arthas/plugin/common/enums/ShellScriptConstantEnum.java
+++ src/com/github/wangji92/arthas/plugin/common/enums/ShellScriptConstantEnum.java
@@ -12,14 +12,15 @@ public enum ShellScriptConstantEnum implements EnumCodeMsg<String> {
     /**
      * https://www.yuque.com/arthas-idea-plugin/help/zkuar3
      */
-    WATCH_SPRING_HTTP("watch org.springframework.web.servlet.DispatcherServlet doService '{params[0].getRequestURI()+\" \"+ #cost}'  -n 5  -x 3 '#cost>1'","watch spring http request cost"),
-    WATCH_SPRING_HTTP_HEADER(" watch org.springframework.web.servlet.DispatcherServlet doService '{params[0].getRequestURI()+\"  header=\"+params[1].getHeaders(\"trace-id\")}'  -n 10  -x 3 -f","watch spring http response header"),
-    WATCH_MYBATIS_BOUND_SQL("watch org.apache.ibatis.mapping.BoundSql getSql '{returnObj,target.parameterObject,throwExp}'  -n 5  -x 3","watch mybatis boundSql"),
-    WATCH_JDBC_REQUEST("watch java.sql.Connection prepareStatement '{params,throwExp}'  -n 5  -x 3  'clazz.getName().startsWith(\"com.mysql\") and params.length==1' and #cost>1 ","watch jdbc sql"),
+    WATCH_SPRING_HTTP("watch org.springframework.web.servlet.DispatcherServlet doService '{params[0].getRequestURI()+\" \"+ #cost}'  -n 5  -x 3 '#cost>1'", "watch spring http request cost"),
+    WATCH_SPRING_HTTP_HEADER(" watch org.springframework.web.servlet.DispatcherServlet doService '{params[0].getRequestURI()+\"  header=\"+params[1].getHeaders(\"trace-id\")}'  -n 10  -x 3 -f", "watch spring http response header"),
+    WATCH_MYBATIS_BOUND_SQL("watch org.apache.ibatis.mapping.BoundSql getSql '{returnObj,target.parameterObject,throwExp}'  -n 5  -x 3", "watch mybatis boundSql"),
+    WATCH_JDBC_REQUEST("watch java.sql.Connection prepareStatement '{params,throwExp}'  -n 5  -x 3  'clazz.getName().startsWith(\"com.mysql\") and params.length==1' and #cost>1 ", "watch jdbc sql"),
     VM_TOOL_FORCE_GC("vmtool --action forceGc", "force gc"),
     BATCH_THREAD("thread -i 3000 -n 5;thread -b;thread --state BLOCKED;", "batch thread"),
-    PROFILER("profiler start --event cpu --interval 10000000 --format jfr -duration 180", "采集cpu jfr，支持jfr格式的工具来查看,JDK Mission Control or JProfiler 执行 180秒自动结束"),
-    PROFILER_ALLOC("profiler start --event alloc --interval 10000000 --threads --format svg -duration 180 --threads", "采集alloc 火焰图 执行 180秒自动结束"),
+    VMTOOL_INTERRUPT_THREAD("vmtool --action interruptThread -t 1", "vmtool interrupt thread by thread id(thread command get)"),
+    PROFILER("profiler start --event cpu --interval 10000000 --format jfr --duration 180", "采集cpu jfr，支持jfr格式的工具来查看,JDK Mission Control or JProfiler 执行 180秒自动结束"),
+    PROFILER_ALLOC("profiler start --event alloc --interval 10000000 --threads --format svg --duration 180 --threads", "采集alloc 火焰图 执行 180秒自动结束"),
     PROFILER_CPU("profiler start --event cpu --interval 10000000 --threads --format svg -duration 180 --threads", "采集cpu 火焰图 执行 180秒自动结束"),
     HEAPDUMP("heapdump /tmp/dump.hprof", "heap dump,heapdump --live /tmp/dump.hprof"),
     JVM("jvm", "display the target jvm information"),
@@ -27,9 +28,9 @@ public enum ShellScriptConstantEnum implements EnumCodeMsg<String> {
      * Arthas发布3.5.6版本：应用排包瘦身不再烦恼
      * https://mp.weixin.qq.com/s?__biz=MzkxNDI0ODE0NQ==&mid=2247484030&idx=1&sn=d5314772a68f587b45ff550a11224c51&chksm=c1701ac0f60793d624bcda54340e61f78587bd8081c76417d9a7e3ea547cc40305a9f00e8410&scene=178&cur_album_id=1896318852766973955#rd
      */
-    CLASSLOADER_URL_STAT("classloader --url-stat","URL使用统计功能，方便排除未使用jar包,注意 不代表Unused URLs可以从应用中删掉。"),
+    CLASSLOADER_URL_STAT("classloader --url-stat", "URL使用统计功能，方便排除未使用jar包,注意 不代表Unused URLs可以从应用中删掉。"),
     CLASSLOADER("classloader;classloader -l;classloader -t;", "show classloader info"),
-    MEMORY("memory","memory命令可以查看JVM内存信息"),
+    MEMORY("memory", "memory命令可以查看JVM内存信息"),
     MBEAN_MEMORY_THREAD("mbean java.lang:type=Memory;mbean java.lang:type=MemoryPool,name=* Usage;mbean java.nio:type=BufferPool,name=*;mbean java.lang:type=Threading;mbean java.lang:type=OperatingSystem;", "use mbean to show memory pool and thread"),
     MBEAN_GARBAGE("mbean java.lang:type=GarbageCollector,name=*;mbean java.lang:type=Compilation;", "use mbean to show garbage and compilation"),
     OPTIONS_UN_SAFE("options unsafe true", "支持对系统级别的类进行增强，打开该开关可能导致把JVM搞挂"),
