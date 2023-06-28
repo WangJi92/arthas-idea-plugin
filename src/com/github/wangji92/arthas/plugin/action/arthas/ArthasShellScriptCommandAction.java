20a326ade326baf3bed0f659406ff26f2d5c3b0d
wangji <983433479@qq.com>
2023-06-29 00:22:14 +0800

    typo

--- src/com/github/wangji92/arthas/plugin/action/arthas/ArthasShellScriptCommandAction.java
+++ src/com/github/wangji92/arthas/plugin/action/arthas/ArthasShellScriptCommandAction.java
@@ -39,6 +39,6 @@ public class ArthasShellScriptCommandAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         ArthasShellScriptCommandDialog dialog = new ArthasShellScriptCommandDialog(event);
-        dialog.open("Quickly Get Available Command(shell command or command command)");
+        dialog.open("Quickly Get Available Command(shell command or common command)");
     }
 }
