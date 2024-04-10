c0ec87c2b5ae09dc5a0a295802d51da4d469b465
wangji <983433479@qq.com>
2024-04-10 21:06:15 +0800

    remove low version compile api

--- src/com/github/wangji92/arthas/plugin/utils/ClassCompileCompatibleUtils.java
+++ src/com/github/wangji92/arthas/plugin/utils/ClassCompileCompatibleUtils.java
@@ -1,18 +1,11 @@
 package com.github.wangji92.arthas.plugin.utils;
 
 import com.intellij.notification.NotificationType;
-import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.task.ProjectTaskManager;
-import com.intellij.task.ProjectTaskNotification;
-import com.intellij.task.ProjectTaskResult;
-import org.apache.commons.lang3.reflect.MethodUtils;
-import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.function.Consumer;
+import org.jetbrains.concurrency.Promise;
 
 /**
  * 编译工程 代码兼容方案
@@ -31,51 +24,46 @@ public class ClassCompileCompatibleUtils {
      * @param successRunnable
      */
     public static void compile(Project project, VirtualFile[] virtualFileFiles, Runnable successRunnable) {
-        if (ApplicationInfo.getInstance().getBuild().getBaselineVersion() <= 201) {
-            //编译兼容一下代码
-            WriteActionCompatibleUtils.runAndWait(project, () -> {
-                ProjectTaskManager instance = ProjectTaskManager.getInstance(project);
-                ProjectTaskNotification taskNotification = new ProjectTaskNotification() {
-                    @Override
-                    public void finished(@NotNull ProjectTaskResult projectTaskResult) {
-                        int errorCount = 0;
-                        try {
-                            errorCount = (int) MethodUtils.invokeMethod(projectTaskResult, "getErrors");
-                        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                            //ignore
-                        }
-                        if (errorCount > 0) {
-                            NotifyUtils.notifyMessage(project, "File compilation errors (it's best to make sure you compile the whole project at least once before a hot update) This will only partially compile the current file", NotificationType.ERROR);
-                            return;
-                        }
-                        WriteActionCompatibleUtils.runAndWait(project, successRunnable::run);
-                    }
-                };
-                MethodUtils.invokeMethod(instance, "compile", new Object[]{virtualFileFiles, taskNotification}, new Class[]{VirtualFile[].class, ProjectTaskNotification.class});
-            });
-        } else {
-            WriteActionCompatibleUtils.runAndWait(project, () -> {
-                ProjectTaskManager instance = ProjectTaskManager.getInstance(project);
-                Object promise = MethodUtils.invokeMethod(instance, "compile", new Object[]{virtualFileFiles}, new Class[]{VirtualFile[].class});
-                MethodUtils.invokeMethod(promise, "onSuccess", (Consumer) o -> {
-                    boolean result = false;
-                    try {
-                        result = (Boolean) MethodUtils.invokeExactMethod(o, "hasErrors");
-                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                        //ignore
-                    }
-                    if (Boolean.FALSE.equals(result)) {
-                        WriteActionCompatibleUtils.runAndWait(project, successRunnable::run);
-                    } else {
-                        NotifyUtils.notifyMessage(project, "File compilation errors (it's best to make sure you compile the whole project at least once before a hot update) This will only partially compile the current file", NotificationType.ERROR);
-                    }
-                });
-                MethodUtils.invokeMethod(promise, "onError", (Consumer<? super Throwable>) o -> {
-                    LOG.error("invoke runAndWait error", o.getCause());
+//        if (ApplicationInfo.getInstance().getBuild().getBaselineVersion() <= 201) {
+//            //编译兼容一下代码
+//            WriteActionCompatibleUtils.runAndWait(project, () -> {
+//                ProjectTaskManager instance = ProjectTaskManager.getInstance(project);
+//                ProjectTaskNotification taskNotification = new ProjectTaskNotification() {
+//                    @Override
+//                    public void finished(@NotNull ProjectTaskResult projectTaskResult) {
+//                        int errorCount = 0;
+//                        try {
+//                            errorCount = (int) MethodUtils.invokeMethod(projectTaskResult, "getErrors");
+//                        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+//                            //ignore
+//                        }
+//                        if (errorCount > 0) {
+//                            NotifyUtils.notifyMessage(project, "File compilation errors (it's best to make sure you compile the whole project at least once before a hot update) This will only partially compile the current file", NotificationType.ERROR);
+//                            return;
+//                        }
+//                        WriteActionCompatibleUtils.runAndWait(project, successRunnable::run);
+//                    }
+//                };
+//                MethodUtils.invokeMethod(instance, "compile", new Object[]{virtualFileFiles, taskNotification}, new Class[]{VirtualFile[].class, ProjectTaskNotification.class});
+//            });
+//        } else {
+        WriteActionCompatibleUtils.runAndWait(project, () -> {
+            ProjectTaskManager instance = ProjectTaskManager.getInstance(project);
+            Promise<ProjectTaskManager.Result> compilePromise = instance.compile(virtualFileFiles);
+            compilePromise.onSuccess(result -> {
+                boolean hasErrors = result.hasErrors();
+                if (hasErrors) {
                     NotifyUtils.notifyMessage(project, "File compilation errors (it's best to make sure you compile the whole project at least once before a hot update) This will only partially compile the current file", NotificationType.ERROR);
-                });
+                } else {
+                    WriteActionCompatibleUtils.runAndWait(project, successRunnable::run);
+                }
             });
-        }
+            compilePromise.onError(throwable -> {
+                LOG.error("compile error", throwable.getCause());
+                NotifyUtils.notifyMessage(project, "File compilation errors (it's best to make sure you compile the whole project at least once before a hot update) This will only partially compile the current file", NotificationType.ERROR);
+            });
+        });
+//        }
     }
 
 
