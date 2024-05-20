bd4daf4c9ccdcc2293282b285e146bb3eeb79a5a
wangji <983433479@qq.com>
2024-05-20 23:58:32 +0800

    modify psi

--- src/com/github/idea/json/parser/model/JPsiTypeClazz.java
+++ src/com/github/idea/json/parser/model/JPsiTypeClazz.java
@@ -5,33 +5,40 @@ import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiType;
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
- *  PsiClass 类型的信息
+ * PsiClass 类型的信息
+ *
  * @author wangji
  * @date 2024/5/19 22:10
  */
 @Getter
+@Deprecated
 public class JPsiTypeClazz extends JPsiTypeBase {
 
     private final PsiClass psiClass;
 
-    public JPsiTypeClazz(PsiClass psiClass, Map<String, PsiType> psiClassGenerics) {
-        this(psiClass, psiClassGenerics, null, 0);
-    }
+    /**
+     * 当前类+所有的父类的名字集合
+     */
+    public Set<String> parentPlusCurrentQualifiedNames;
+
 
     public JPsiTypeClazz(PsiClass psiClass, Map<String, PsiType> psiClassGenerics, List<String> ignoreProperties, int recursionLevel) {
         this.psiClass = psiClass;
-        if (ignoreProperties == null) {
-            this.ignoreProperties = List.of();
-        } else {
+        // 获取父类的信息
+        Set<String> clazzNames = Arrays.stream(psiClass.getSuperTypes()).map(PsiType::getCanonicalText).collect(Collectors.toSet());
+        clazzNames.add(psiClass.getQualifiedName());
+        this.parentPlusCurrentQualifiedNames = clazzNames;
+        if (ignoreProperties != null) {
             this.ignoreProperties = ignoreProperties;
         }
-        if (psiClassGenerics == null) {
-            this.psiTypeGenerics = Map.of();
-        } else {
+        if (psiClassGenerics != null) {
             this.psiTypeGenerics = psiClassGenerics;
         }
         this.recursionLevel = recursionLevel;
